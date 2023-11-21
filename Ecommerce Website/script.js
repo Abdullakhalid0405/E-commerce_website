8ce284e729a2fc27f167ffcd089a900c0d3084f2
Abdullakhalid0405 <151620507+Abdullakhalid0405@users.noreply.github.com>
2023-11-21 15:01:22 +0530

    Add files via upload

---
+++ Ecommerce Website/script.js
@@ -0,0 +1,80 @@
+const bar = document.getElementById('bar');
+const nav = document.getElementById('navbar');
+
+if (bar) {
+    bar.addEventListener('click',() => {
+         nav.classList.add('active');
+    })
+}
+
+var timeout;
+
+const scroll = new LocomotiveScroll({
+    el: document.querySelector("body"),
+    smooth: true,
+  });
+
+
+function firstPageAnim() {
+    var tl = gsap.timeline();
+  
+    tl.from(".horizontal-list text-center nav-menu", {
+      y: "-10",
+      opacity: 0,
+      duration: 1.5,
+      ease: Expo.easeInOut,
+    })
+      .to(".boundingelem", {
+        y: 0,
+        ease: Expo.easeInOut,
+        duration: 2,
+        delay: -1,
+        stagger: 0.2,
+    })
+      .from("#herofooter", {
+        y: -10,
+        opacity: 0,
+        duration: 1.5,
+        delay: -1,
+        ease: Expo.easeInOut,
+    });
+}
+
+const cursor = new MouseFollower({
+    el: null,
+    container: document.body,
+    className: 'mf-cursor',
+    innerClassName: 'mf-cursor-inner',
+    textClassName: 'mf-cursor-text',
+    mediaClassName: 'mf-cursor-media',
+    mediaBoxClassName: 'mf-cursor-media-box',
+    iconSvgClassName: 'mf-svgsprite',
+    iconSvgNamePrefix: '-',
+    iconSvgSrc: '',
+    dataAttr: 'cursor',
+    hiddenState: '-hidden',
+    textState: '-text',
+    iconState: '-icon',
+    activeState: '-active',
+    mediaState: '-media',
+    stateDetection: {
+        '-pointer': 'a,button',
+        '-hidden': 'iframe'
+    },
+    visible: true,
+    visibleOnState: false,
+    speed: 0.55,
+    ease: 'expo.out',
+    overwrite: true,
+    skewing: 0,
+    skewingText: 2,
+    skewingIcon: 2,
+    skewingMedia: 2,
+    skewingDelta: 0.001,
+    skewingDeltaMax: 0.15,
+    stickDelta: 0.15,
+    showTimeout: 20,
+    hideOnLeave: true,
+    hideTimeout: 300,
+    hideMediaTimeout: 300
+});
